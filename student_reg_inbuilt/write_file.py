7b6063a6540d7dd4ce4ec456d4d605615a454336
Aftab Alam <132725670+Aftab780879@users.noreply.github.com>
2025-12-15 09:42:56 +0530

    Delete student_reg_inbuilt directory

--- student_reg_inbuilt/write_file.py
+++
@@ -1,7 +0,0 @@
-import json
-def write_json(json_file,student_list):
-    try:
-        with open(json_file,'w') as file:
-                    file.write(json.dumps(student_list,indent=4))
-    except Exception:
-        print("\nServer is Busy , Try After some time")                  
